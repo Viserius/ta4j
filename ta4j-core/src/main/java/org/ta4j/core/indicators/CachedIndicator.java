7f02e642233c537b6dffe06a7ffd287f89776e78
Mark Soelman <info@marksoelman.nl>
2021-03-22 21:18:46 +0100

    CachedIndicator update for concurrency

--- ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
@@ -69,6 +69,8 @@ public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
         this(indicator.getBarSeries());
     }
 
+    private boolean sizeInitialized = false;
+
     @Override
     public T getValue(int index) {
         BarSeries series = getBarSeries();
@@ -79,50 +81,26 @@ public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
             return calculate(index);
         }
 
-        // Series is not null
+        if(!this.sizeInitialized) {
+            initializeLength();
+        }
 
-        final int removedBarsCount = series.getRemovedBarsCount();
-        final int maximumResultCount = series.getMaximumBarCount();
-
-        T result;
-        if (index < removedBarsCount) {
-            // Result already removed from cache
-            log.trace("{}: result from bar {} already removed from cache, use {}-th instead",
-                    getClass().getSimpleName(), index, removedBarsCount);
-            increaseLengthTo(removedBarsCount, maximumResultCount);
-            highestResultIndex = removedBarsCount;
-            result = results.get(0);
-            if (result == null) {
-                // It should be "result = calculate(removedBarsCount);".
-                // We use "result = calculate(0);" as a workaround
-                // to fix issue #120 (https://github.com/mdeverdelhan/ta4j/issues/120).
-                result = calculate(0);
-                results.set(0, result);
-            }
-        } else {
-            if (index == series.getEndIndex()) {
-                // Don't cache result if last bar
-                result = calculate(index);
-            } else {
-                increaseLengthTo(index, maximumResultCount);
-                if (index > highestResultIndex) {
-                    // Result not calculated yet
-                    highestResultIndex = index;
-                    result = calculate(index);
-                    results.set(results.size() - 1, result);
-                } else {
-                    // Result covered by current cache
-                    int resultInnerIndex = results.size() - 1 - (highestResultIndex - index);
-                    result = results.get(resultInnerIndex);
-                    if (result == null) {
-                        result = calculate(index);
-                        results.set(resultInnerIndex, result);
-                    }
-                }
-            }
-
+        T result = this.results.get(index);
+        if (result == null) {
+            result = calculate(index);
+            results.set(index, result);
+            if(index > this.highestResultIndex)
+                this.highestResultIndex = index;
         }
         return result;
+    }
+
+    private synchronized void initializeLength() {
+        if(!this.sizeInitialized) {
+            assert results.isEmpty() : "Cache results list should be empty";
+            results.addAll(Collections.nCopies(this.getBarSeries().getBarData().size(), null));
+            this.sizeInitialized = true;
+        }
     }
 
     /**
@@ -130,44 +108,4 @@ public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
      * @return the value of the indicator
      */
     protected abstract T calculate(int index);
-
-    /**
-     * Increases the size of cached results buffer.
-     *
-     * @param index     the index to increase length to
-     * @param maxLength the maximum length of the results buffer
-     */
-    private void increaseLengthTo(int index, int maxLength) {
-        if (highestResultIndex > -1) {
-            int newResultsCount = Math.min(index - highestResultIndex, maxLength);
-            if (newResultsCount == maxLength) {
-                results.clear();
-                results.addAll(Collections.nCopies(maxLength, null));
-            } else if (newResultsCount > 0) {
-                results.addAll(Collections.nCopies(newResultsCount, null));
-                removeExceedingResults(maxLength);
-            }
-        } else {
-            // First use of cache
-            assert results.isEmpty() : "Cache results list should be empty";
-            results.addAll(Collections.nCopies(Math.min(index + 1, maxLength), null));
-        }
-    }
-
-    /**
-     * Removes the N first results which exceed the maximum bar count. (i.e. keeps
-     * only the last maximumResultCount results)
-     *
-     * @param maximumResultCount the number of results to keep
-     */
-    private void removeExceedingResults(int maximumResultCount) {
-        int resultCount = results.size();
-        if (resultCount > maximumResultCount) {
-            // Removing old results
-            final int nbResultsToRemove = resultCount - maximumResultCount;
-            for (int i = 0; i < nbResultsToRemove; i++) {
-                results.remove(0);
-            }
-        }
-    }
 }
